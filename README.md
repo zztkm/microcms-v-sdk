2d409875f2d3f908b14c8603455fcbac9f47cfb0
zztkm <takumi.basket1682@gmail.com>
2022-09-28 03:08:59 +0900

    :sparkles: content api

--- README.md
+++ README.md
@@ -22,15 +22,18 @@ import zztkm.microcms
 
 TODO: 使い方をがんばって記載する
 
-## TODO
+## APIs
 
-Contents API
-- [ ] [GET /api/v1/{endpoint}](https://document.microcms.io/content-api/get-list-contents)
-- [ ] [GET /api/v1/{endpoint}/{content_id}](https://document.microcms.io/content-api/get-content)
+Content API
+- [x] [GET /api/v1/{endpoint}](https://document.microcms.io/content-api/get-list-contents)
+- [x] [GET /api/v1/{endpoint}/{content_id}](https://document.microcms.io/content-api/get-content)
 - [ ] [POST /api/v1/{endpoint}](https://document.microcms.io/content-api/post-content)
 - [ ] [PUT /api/v1/{endpoint}/{content_id}](https://document.microcms.io/content-api/put-content)
 - [ ] [PATCH /api/v1/{endpoint}/{content_id}](https://document.microcms.io/content-api/patch-content)
 - [ ] [DELETE /api/v1/{endpoint}/{content_id}](https://document.microcms.io/content-api/delete-content)
 
 Management API
-- [ ] 
+- [ ] [GET /api/v1/contents/{endpoint}](https://document.microcms.io/management-api/get-list-contents-management)
+- [ ] [GET /api/v1/contents/{endpoint}/{content_id}](https://document.microcms.io/management-api/get-content)
+- [ ] [PATCH /api/v1/contents/{endpoint}/{content_id}/status](https://document.microcms.io/management-api/patch-contents-status)
+- [ ] [GET /api/v1/media](https://document.microcms.io/management-api/get-media)
--- contents.v
+++ contents.v
@@ -3,65 +3,130 @@ module microcms
 import json
 import net.urllib
 
-pub struct ListParams {
-pub mut:
-	endpoint string
-	draft_key string
-	limit    int
-	offset   int
-	orders   []string
-	q        string
-	fields   []string
-	ids      []string
-	filters  string
-	depth    int
-}
-
-pub struct GetParams {
+pub struct GetContentListParams {
 pub mut:
 	endpoint  string
+	draft_key string
+	limit     int
+	offset    int
+	orders    []string
+	q         string
+	fields    []string
+	ids       []string
+	filters   string
+	depth     int
+	rich_editor_format string
+}
+
+pub struct GetContentParams {
+pub mut:
+	endpoint   string
 	content_id string
 	draft_key  string
-	fields    []string
-	depth     int
+	fields     []string
+	depth      int
+	rich_editor_format string
 }
 
-fn make_list_query(p ListParams) urllib.Values {
+pub struct CreateParams {
+pub mut:
+	endpoint   string
+	content_id string
+	status     string
+}
+
+pub struct UpdateParams {
+pub mut:
+	endpoint   string
+	content_id string
+}
+
+pub struct DeleteParams {
+pub mut:
+	endpoint   string
+	content_id string
+}
+
+pub struct CreateResponse {
+pub:
+	id string
+}
+
+pub struct UpdateResponse {
+pub:
+	id string
+}
+
+fn make_list_query(p GetContentListParams) urllib.Values {
 	mut v := urllib.new_values()
 	if len(p.draft_key) > 0 {
-		v.add("draftKey", p.draft_key)
+		v.add('draftKey', p.draft_key)
 	}
 	if p.Limit != 0 {
-		v.add("limit", p.Limit.str())
+		v.add('limit', p.Limit.str())
 	}
 	if p.offset != 0 {
-		v.add("offset", p.offset.str())
+		v.add('offset', p.offset.str())
 	}
 	if len(p.orders) > 0 {
-		v.add("orders", p.orders.join(","))
+		v.add('orders', p.orders.join(','))
 	}
 	if len(p.q) > 0 {
-		v.add("q", p.q)
+		v.add('q', p.q)
 	}
 	if len(p.fields) > 0 {
-		v.add("fields", p.fields.join(","))
+		v.add('fields', p.fields.join(','))
 	}
 	if len(p.IDs) > 0 {
-		v.add("ids", p.ids.join(","))
+		v.add('ids', p.ids.join(','))
 	}
 	if len(p.filters) > 0 {
-		v.add("filters", p.filters)
+		v.add('filters', p.filters)
 	}
 	if p.depth != 0 {
-		v.add("depth", p.depth.str())
+		v.add('depth', p.depth.str())
 	}
-
+	if len(p.rich_editor_format) > 0 {
+		v.add('richEditorFormat', v.rich_editor_format)
+	} 
+	return v
 }
 
-pub fn (c Client) list<T>(p ListParams) ?T {
+fn make_get_query(p GetContentParams) urllib.Values {
+	mut v := urllib.new_values()
+	if len(p.draft_key) > 0 {
+		v.add('draftKey', p.draft_key)
+	}
+	if len(p.fields) > 0 {
+		v.add('fields', p.fields.join(','))
+	}
+	if p.depth != 0 {
+		v.add('depth', p.depth.str())
+	}
+	if len(p.rich_editor_format) > 0 {
+		v.add('richEditorFormat', v.rich_editor_format)
+	} 
+	return v
+}
+
+pub fn (c Client) content_list<T>(p GetContentListParams) ?T {
 	req := new_request(.get, p.endpoint, make_list_query(p))?
 
 	res := send_request(req)?
-	
+
 	return json.decode(T, res.body)
 }
+
+pub fn (c Client) content<T>(p GetContentParams) ?T {
+	req := new_request(.get, p.endpoint, make_get_query(p))?
+
+	res := send_request(req)?
+
+	return json.decode(T, res.body)
+}
+
+pub fn (c Client) create_content<T>(p CreateParams, data T) ?CreateResponse {}
+
+pub fn (c Client) update_content<T>(p UpdateParams, data T) ?UpdateResponse {}
+
+pub fn (c Client) delete_content(p DeleteParams) ? {}
