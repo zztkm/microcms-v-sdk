15717ebf11f281417461ad788073c275db3b9147
zztkm <takumi.basket1682@gmail.com>
2022-09-29 01:08:20 +0900

    update sample code

--- sample/main.v
+++ sample/main.v
@@ -1,14 +1,8 @@
 module main
 
-import json
 import os
-import net.http
+import zztkm.microcms
 import zztkm.vdotenv
-
-const (
-	base_domain = 'microcms.io'
-	api_version = 'v1'
-)
 
 struct Content {
 	id           string
@@ -27,44 +21,26 @@ struct BlogList {
 	limit       int
 }
 
-fn list<T>() ?T {
-
-	service_domain := os.getenv('SERVICE_DOMAIN')
-	api_key := os.getenv('MICROCMS_API_KEY')
-
-	endpoint := 'blogs'
-
-	url := 'https://$service_domain' + '.' + base_domain + '/api/$api_version/$endpoint'
-
-	println("api url: $url")
-
-	mut req := http.Request {
-		method: .get
-		url: url
-	}
-	req.add_custom_header('X-MICROCMS-API-KEY', api_key)?
-
-	res := req.do()?
-	println("status: $res.status_code.str()")
-	if res.status_code >= 400 {
-		return error(res.body)
-	}
-
-	return json.decode(T, res.body)
-}
-
 fn main() {
 	// shell environment variables take precedence
 	vdotenv.load()
+	service_domain := os.getenv('SERVICE_DOMAIN')
+	api_key := os.getenv('MICROCMS_API_KEY')
 
-	blogs := list<BlogList>() or {
+	c := microcms.new(service_domain, api_key)
+
+	param := microcms.GetContentListParams{
+		endpoint: 'blogs'
+	}
+
+	blogs := c.content_list<BlogList>(param) or {
 		eprintln(err)
 		return
 	}
 
-	println("total contents count: $blogs.total_count")
+	println('total contents count: $blogs.total_count')
 
-	println("--- print contents title ---")
+	println('--- print contents title ---')
 	for content in blogs.contents {
 		println(content.title)
 	}
